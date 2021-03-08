582f7fdfee3196820a29974da1707df2dbaf0408
Stackbit <projects@stackbit.com>
2021-03-08 14:59:19 +0000

    initial commit by Stackbit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,12 @@
+import 'focus-visible'
+
+import './modules/navigation'
+import './modules/signup'
+import './modules/lazyload'
+import './modules/themepicker'
+import './modules/infinitescroll'
+import './modules/preload'
+import './modules/speedlify'
+import './modules/register-serviceworker'
+
+document.documentElement.classList.remove('no-js')
